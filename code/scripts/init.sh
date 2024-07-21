61a2b58542e0ec4a75d2b2aac391fa6ab647297b
Haitham Alhaji (mac) <haithamtalhaji@yahoo.com>
2024-07-21 21:33:32 +0300

    batch

--- code/scripts/init.sh
+++ code/scripts/init.sh
@@ -17,10 +17,13 @@ pip install gdown
 sudo apt-get install unzip
 
 mkdir downloaded_files
+mkdir -p data/birds
 gdown -O downloaded_files/preprocessed_birds.zip --id 1I6ybkR7L64K8hZOraEZDuHh0cCJw5OUj
-unzip -oq downloaded_files/preprocessed_birds.zip -d GANCLIP/data
+unzip -oq downloaded_files/preprocessed_birds.zip -d data
 
 wget -O downloaded_files/birds.tgz https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz
-tar xvzf downloaded_files/birds.tgz -C GANCLIP/data/birds
+tar xvzf downloaded_files/birds.tgz -C data/birds
 
 python code/src/train.py --cfg code/cfg/birds.yml
+
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=11266 code/src/train.py --cfg code/cfg/birds.yml
