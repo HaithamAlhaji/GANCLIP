96217518a396907763e7714648e227ac96bdc169
Haitham Alhaji (mac) <haithamtalhaji@yahoo.com>
2024-07-21 20:15:10 +0300

    init -y

--- code/scripts/init.sh
+++ code/scripts/init.sh
@@ -4,14 +4,14 @@
 
 # git fetch --all
 # git reset --hard origin/main
-conda create --name myenv python=3.9
+
+conda create --name myenv python=3.9 -y
 conda activate myenv
 
 pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip install -r GANCLIP/requirements.txt
+pip install -r requirements.txt
 pip install git+https://github.com/openai/CLIP.git
 pip install setuptools==59.5.0
-pip uninstall numpy -y
 pip install numpy
 pip install gdown
 sudo apt-get install unzip
@@ -23,5 +23,4 @@ unzip -oq downloaded_files/preprocessed_birds.zip -d GANCLIP/data
 wget -O downloaded_files/birds.tgz https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz
 tar xvzf downloaded_files/birds.tgz -C GANCLIP/data/birds
 
-cd GANCLIP
 python code/src/train.py --cfg code/cfg/birds.yml
