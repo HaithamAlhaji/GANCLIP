b709b506dcb48f3257086eb7aa48b7f480b2de47
haitham_seerver <haithamtalhaji@yahoo.com>
2024-07-29 16:37:58 +0000

    update

--- code/scripts/init_server.sh
+++ code/scripts/init_server.sh
@@ -1,6 +1,7 @@
 # git clone https://github.com/HaithamAlhaji/GANCLIP.git && cd GANCLIP/ && bash ./code/scripts/init_server.sh
 
-
+git config --global user.name "haitham_seerver"
+git config user.email "haithamtalhaji@yahoo.com"
 
 pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
